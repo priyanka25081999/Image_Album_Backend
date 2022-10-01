1383bc15cd9e7b25a05a433e7df5c38bac86e851
Priyanka Salunke <salunkeps99@gmail.com>
2022-10-01 19:44:50 +0530

    Image and bucket create and list done

--- routes/AlbumRoute.js
+++ routes/AlbumRoute.js
@@ -10,17 +10,46 @@ s3 = new aws.S3({ apiVersion: "2006-03-01" });
 albumRouter.get("/", async (req, res) => {
   await s3.listBuckets(function (err, data) {
     if (err) {
-      console.log("Error", err);
-      res.status(500).json({ isDone: false, error: err });
+      console.log("Get Buckets: Error", err);
+      return res.status(500).json({ isDone: false, error: err });
     } else {
-      console.log("Success", data.Buckets);
-      res.status(200).json({ isDone: true, buckets: data.Buckets });
+      console.log("Get Buckets: Success", data.Buckets);
+      return res.status(200).json({ isDone: true, buckets: data.Buckets });
     }
   });
 });
 
 albumRouter.put("/", (req, res) => {
-  res.send("Ambum PUT Route");
+  if (!req.body.hasOwnProperty("bucket")) {
+    return res.status(400).json({
+      isDone: false,
+      error: {
+        message: "Please enter bucket name",
+      },
+    });
+  }
+  if (req.body.bucket.trim() === "") {
+    return res.status(400).json({
+      isDone: false,
+      error: {
+        message: "Please enter valid bucket name",
+      },
+    });
+  }
+  var bucketParams = {
+    Bucket: req.body.bucket,
+    ACL: "public-read",
+  };
+
+  s3.createBucket(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Create Bucket: Error", err);
+      return res.status(500).json({ isDone: false, error: err });
+    } else {
+      console.log("Create Bucket: Success", data.Location);
+      return res.status(200).json({ isDone: true, result: data.Location });
+    }
+  });
 });
 
 module.exports = albumRouter;
--- routes/ImageRoute.js
+++ routes/ImageRoute.js
@@ -1,13 +1,86 @@
 const express = require("express");
+const aws = require("aws-sdk");
 
 const imageRouter = express.Router();
 
+// AWS SDK
+aws.config.update({ region: "us-east-1" });
+s3 = new aws.S3({ apiVersion: "2006-03-01" });
+
 imageRouter.get("/", (req, res) => {
-  res.send("Image GET Route");
+  if (!req.body.hasOwnProperty("bucket")) {
+    return res.status(500).json({
+      isDone: false,
+      error: {
+        message: "Please enter bucket name",
+      },
+    });
+  }
+
+  if (req.body.bucket.trim() === "") {
+    return res.status(500).json({
+      isDone: false,
+      error: {
+        message: "Please enter valid bucket name",
+      },
+    });
+  }
+
+  var bucketParams = {
+    Bucket: req.body.bucket,
+  };
+
+  s3.listObjects(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+      return res.status(500).json({ isDone: false, error: err });
+    } else {
+      console.log("Success", data);
+      return res.status(200).json({ isDone: true, result: data });
+    }
+  });
 });
 
 imageRouter.put("/", (req, res) => {
-  res.send("Image PUT Route");
+  if (!req.body.hasOwnProperty("bucket")) {
+    return res.status(500).json({
+      isDone: false,
+      error: {
+        message: "Please enter bucket name",
+      },
+    });
+  }
+
+  if (req.body.bucket.trim() === "") {
+    return res.status(500).json({
+      isDone: false,
+      error: {
+        message: "Please enter valid bucket name",
+      },
+    });
+  }
+
+  const bucket = req.body.bucket;
+  const imageName = req.body.imageName;
+  const imageBody = req.body.imageBody;
+
+  var uploadParams = {
+    Bucket: bucket,
+    Key: imageName,
+    Body: imageBody,
+    ACL: "public-read",
+  };
+
+  s3.upload(uploadParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+      return res.status(500).json({ isDone: false, error: err });
+    }
+    if (data) {
+      console.log("Upload Success", data.Location);
+      return res.status(200).json({ isDone: true, result: data.Location });
+    }
+  });
 });
 
 module.exports = imageRouter;
