1326af0b83c20643db773e63a14cd0d8ce5a38a3
Priyanka Salunke <salunkeps99@gmail.com>
2022-10-02 19:37:21 +0530

    Bucket (Album) delete functionality added

--- routes/AlbumRoute.js
+++ routes/AlbumRoute.js
@@ -52,4 +52,36 @@ albumRouter.put("/", (req, res) => {
   });
 });
 
+albumRouter.delete("/", (req, res) => {
+  if (!req.query.hasOwnProperty("bucket")) {
+    return res.status(400).json({
+      isDone: false,
+      error: {
+        message: "Please enter bucket name",
+      },
+    });
+  }
+  if (req.query.bucket.trim() === "") {
+    return res.status(400).json({
+      isDone: false,
+      error: {
+        message: "Please enter valid bucket name",
+      },
+    });
+  }
+  var bucketParams = {
+    Bucket: req.query.bucket,
+  };
+
+  s3.deleteBucket(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Delete Bucket: Error", err);
+      return res.status(500).json({ isDone: false, error: err });
+    } else {
+      console.log("Delete Bucket: Success", data);
+      return res.status(200).json({ isDone: true, result: data });
+    }
+  });
+});
+
 module.exports = albumRouter;
