052c9074e549e537235f81f0a6a9ebfc4a657a2a
Priyanka Salunke <salunkeps99@gmail.com>
2022-10-04 18:21:38 +0530

    Image API

---
+++ routes/ImageRoute.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const aws = require("aws-sdk");
+
+const imageRouter = express.Router();
+
+// AWS SDK
+aws.config.update({ region: "us-east-1" });
+s3 = new aws.S3({ apiVersion: "2006-03-01" });
+
+imageRouter.get("/", (req, res) => {
+  const bucket = req.query.bucket;
+
+  if (bucket === undefined) {
+    return res.status(500).json({
+      isDone: false,
+      error: {
+        message: "Please enter bucket name",
+      },
+    });
+  }
+
+  if (bucket.trim() === "") {
+    return res.status(500).json({
+      isDone: false,
+      error: {
+        message: "Please enter valid bucket name",
+      },
+    });
+  }
+
+  var bucketParams = {
+    Bucket: bucket,
+  };
+
+  s3.listObjects(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+      return res.status(500).json({ isDone: false, error: err });
+    } else {
+      console.log("Success", data);
+      return res.status(200).json({ isDone: true, result: data.Contents });
+    }
+  });
+});
+
+imageRouter.post("/", (req, res) => {
+  const file = req.files.file;
+  var uploadParams = {
+    Bucket: req.body.bucket,
+    Key: file.name,
+    Body: file.data,
+    ACL: "public-read",
+  };
+
+  s3.upload(uploadParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+      return res.status(500).json({ isDone: false, error: err });
+    }
+    if (data) {
+      console.log("Upload Success", data.Location);
+      return res.status(200).json({ isDone: true, result: data.Location });
+    }
+  });
+});
+
+imageRouter.delete("/", (req, res) => {
+  const bucket = req.query.bucket;
+  const object = req.query.key;
+
+  if (bucket === undefined || object === undefined) {
+    return res.status(500).json({
+      isDone: false,
+      error: {
+        message: "Please enter valid data",
+      },
+    });
+  }
+
+  if (bucket.trim() === "" || bucket.trim() === "") {
+    return res.status(500).json({
+      isDone: false,
+      error: {
+        message: "Please enter valid name",
+      },
+    });
+  }
+
+  var bucketParams = {
+    Bucket: bucket,
+    Key: object,
+  };
+
+  s3.deleteObject(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+      return res.status(500).json({ isDone: false, error: err });
+    } else {
+      console.log("Success", data);
+      return res.status(200).json({ isDone: true, result: data });
+    }
+  });
+});
+
+module.exports = imageRouter;
